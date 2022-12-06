67d0dfc30369b65ff6f3c4ecaf07704ab6cf77b7
Joey Talbot <joeytalbot85@gmail.com>
2022-12-06 16:49:43 +0000

    rnets

--- code/censusdata.R
+++ code/censusdata.R
@@ -34,12 +34,22 @@ lines_car = lines %>%
   filter(car_driver > 0) %>% 
   select(-bicycle, -foot, -c(govtarget_slc:ebike_sico2))
 
+
+# Unjittered routes ------------------------------------------------------
+
 library(stplanr)
 # coords = od_coords(lines)
 # from = coords[, 1:2]
 # to = coords[, 3:4]
-# routes_osrm = route_osrm(from = from, to = to, osrm.profile = "foot") # not working
-# lines_some = head(lines)
+# routes_osrm = route_osrm(from = from, to = to, osrm.profile = "foot") # not working]
+
+# Only does one route at a time
+# library(osrm)
+# from = as.data.frame(from)
+# to = as.data.frame(to)
+# r_osrm = osrmRoute(src = from, dst = to, osrm.server = "https://routing.openstreetmap.de/", 
+#                    osrm.profile = "foot")
+
 foot_osrm = route(l = lines_foot, route_fun = route_osrm) # default routing profile is "foot"
 bicycle_osrm = route(l = lines_bicycle, route_fun = route_osrm, osrm.profile = "bike")
 car_osrm = route(l = lines_car, route_fun = route_osrm, osrm.profile = "car")
@@ -49,34 +59,36 @@ saveRDS(bicycle_osrm, "data/bicycle_osrm.Rds")
 saveRDS(car_osrm, "data/car_osrm.Rds")
 
 foot_osrm = readRDS("data/foot_osrm.Rds")
+bicycle_osrm = readRDS("data/bicycle_osrm.Rds")
+car_osrm = readRDS("data/car_osrm.Rds")
 
 # foot_some = head(foot_osrm)
 tm_shape(foot_osrm) + tm_lines("foot")
 tm_shape(bicycle_osrm) + tm_lines("bicycle")
-tm_shape(car_osrm) + tm_lines("car")
-
-# Only does one route at a time
-# library(osrm)
-# from = as.data.frame(from)
-# to = as.data.frame(to)
-# r_osrm = osrmRoute(src = from, dst = to, osrm.server = "https://routing.openstreetmap.de/", 
-#                    osrm.profile = "foot")
-
-rnet = get_pct_rnet(region = "north-east", purpose = "commute", geography = "lsoa")
-rnet = rnet[tyneandwear, ]
-tm_shape(rnet) + tm_lines("bicycle")
+tm_shape(car_osrm) + tm_lines("car_driver")
 
 
+# Route networks ----------------------------------------------------------
 
-# l1 = od_data_lines[49, ]
-# l1m = od_coords(l1)
-# from = l1m[, 1:2]
-# to = l1m[, 3:4]
-# if(curl::has_internet()) {
-# r_foot = route_osrm(from, to)
-# r_bike = route_osrm(from, to, osrm.profile = "bike")
-# r_car = route_osrm(from, to, osrm.profile = "car")
-# plot(r_foot$geometry, lwd = 9, col = "grey")
-# plot(r_bike, col = "blue", add = TRUE)
-# plot(r_car, col = "red", add = TRUE)
-# }
+foot_rnet = overline(foot_osrm, attrib = "foot")
+bicycle_rnet = overline(bicycle_osrm, attrib = "bicycle")
+car_rnet = overline(car_osrm, 
+                    attrib = c("car_driver", "car_passenger", "motorbike", "taxi_other")
+                    )
+
+saveRDS(foot_rnet, "data/foot_rnet.Rds")
+saveRDS(bicycle_rnet, "data/bicycle_rnet.Rds")
+saveRDS(car_rnet, "data/car_rnet.Rds")
+
+foot_rnet = readRDS("data/foot_rnet.Rds")
+bicycle_rnet = readRDS("data/bicycle_rnet.Rds")
+car_rnet = readRDS("data/car_rnet.Rds")
+
+tm_shape(foot_rnet) + tm_lines("foot")
+tm_shape(bicycle_rnet) + tm_lines("bicycle")
+tm_shape(car_rnet) + tm_lines("car_driver")
+
+# rnet = get_pct_rnet(region = "north-east", purpose = "commute", geography = "lsoa")
+# rnet = rnet[tyneandwear, ]
+# tm_shape(rnet) + tm_lines("bicycle")
+
