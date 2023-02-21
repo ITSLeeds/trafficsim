42b4c69f02939a18504a49e6ebd870a107a786a7
Joey Talbot <joeytalbot85@gmail.com>
2023-02-21 17:35:53 +0000

    osm lanes

--- code/air-pollution.R
+++ code/air-pollution.R
@@ -1,15 +1,9 @@
 library(tidyverse)
 library(tmap)
 tmap_mode("view")
-tmap_options(check.and.fix = TRUE)
 
-# pm10_grouped = readRDS("data/pm10_grouped_2021_1.Rds")
-# View(pm10_grouped)
-# tm_shape(pm10_grouped) + tm_dots("median_value")
-
-
-tm_shape(sensor_stats) + tm_dots("median_value")
-
+pm10 = readRDS("data/pm10_stats_2021_2.Rds")
+traffic = readRDS("data/plates_in_stats_2021_2.Rds")
 
 
 # Checking extreme values -------------------------------------------------
@@ -35,11 +29,41 @@ ggplot(per, aes(x = Timestamp, y = Value)) +
   geom_line() + 
   geom_point()
 
+# OSM data ----------------------------------------------------------------
+
+# Check the number of lanes, road names, road refs, speed limits, etc
+# Use number of lanes to improve the correlation with 2011 Census data?
+# Need to consider whether road is dual carriageway and whether it has bus lanes, 
+# and how many lanes the camera covers
+
+osm = readRDS("data/osm_drive_2023-01-17.Rds")
+
+traffic_osm = st_join(traffic, osm)
+
+nearest = st_nearest_feature(traffic, osm)
+nn = osm[nearest,] %>% 
+  st_drop_geometry()
+traffic_osm = bind_cols(traffic, nn)
+
+tm_shape(traffic_osm) + tm_dots("lanes")
+
+
+per = traffic_osm %>% 
+  filter(`Sensor Name` == "PER_NE_CAJT_GHA167_DR3_DR2A")
+
+
+
+per = traffic_osm %>% 
+  filter(`Sensor Name` == "PER_NE_CAJT_NTA1058_CR4_SR3")
+
+per = traffic_osm %>% 
+  filter(`Sensor Name` == "PER_NE_CAJT_NCA189_SL6_JDR4")
+
+
+# Now check the number of lanes
 
 # Get LSOA/MSOA zones and check for zones with both sensor types ----------
 
-pm10 = readRDS("data/pm10_stats_2021_2.Rds")
-traffic = readRDS("data/plates_in_stats_2021_2.Rds")
 
 library(pct)
 northeast = get_pct_zones("north-east", geography = "msoa")
@@ -64,9 +88,20 @@ join_traffic = st_join(join, traffic) %>%
   filter(!is.na(mean_traffic))
 tm_shape(join_traffic) + tm_polygons("mean_pm10") + 
   tm_shape(pm10) + tm_dots("median_value")
-tm_shape(join_traffic) + tm_polygons("mean_traffic", alpha = 0.5) +
+tm_shape(join_traffic) + tm_polygons("mean_pm10", alpha = 0.5) +
   tm_shape(traffic) + tm_dots("sum_plates", palette = "-magma") + 
   tm_shape(pm10) + tm_bubbles("median_value")
+
+ggplot(join_traffic, aes(mean_traffic, mean_pm10)) + 
+  geom_point()
+m1 = lm(mean_pm10 ~ mean_traffic, data = join_traffic)
+summary(m1)$r.squared
+# [1] 0.05653983
+
+
+# Same with LSOAs ---------------------------------------------------------
+
+# only 14 LSOAs match both datasets
 
 # join = st_join(northeast_lsoa, pm10) %>% 
 #   group_by(geo_code) %>% 
@@ -81,3 +116,5 @@ tm_shape(join_traffic) + tm_polygons("mean_traffic", alpha = 0.5) +
 # tm_shape(join_traffic) + tm_polygons("mean_traffic")
 
 # Get built-up area bounds or calculate population density
+
+
