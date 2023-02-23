517193901693fae50d126a0921db33730a575bba
Joey Talbot <joeytalbot85@gmail.com>
2023-02-23 16:18:49 +0000

    half season

--- code/stadiums.R
+++ code/stadiums.R
@@ -6,9 +6,10 @@ st_james_park = oe_get("St. James' Park")
 
 # Time period around matches ----------------------------------------------
 
-plates_5 = readRDS("data/plates_in_2021_5.Rds")
-plates_6 = readRDS("data/plates_in_2021_6.Rds")
-plates_7 = readRDS("data/plates_in_2021_7.Rds")
+# Half season Aug-Dec 2021
+# plates_5 = readRDS("data/plates_in_2021_5.Rds")
+# plates_6 = readRDS("data/plates_in_2021_6.Rds")
+# plates_7 = readRDS("data/plates_in_2021_7.Rds")
 plates_8 = readRDS("data/plates_in_2021_8.Rds")
 plates_9 = readRDS("data/plates_in_2021_9.Rds")
 plates_10 = readRDS("data/plates_in_2021_10.Rds")
@@ -16,24 +17,46 @@ plates_11 = readRDS("data/plates_in_2021_11.Rds")
 plates_12 = readRDS("data/plates_in_2021_12.Rds")
 
 # Nearby sensors
-
-nearby = plates_5 %>% 
+plates_8 = plates_8 %>% 
   filter(
     `Sensor Name` %in% c("PER_NE_CAJT_NCA189_SJB2_SJB1", "PER_NE_CAJT_NCA189_BR3_GR",
                          "PER_NE_CAJT_NCA189_BR3_SJB2", "PER_NE_CAJT_NCA189_SJB2_BR3"))
+plates_9 = plates_9 %>% 
+  filter(
+    `Sensor Name` %in% c("PER_NE_CAJT_NCA189_SJB2_SJB1", "PER_NE_CAJT_NCA189_BR3_GR",
+                         "PER_NE_CAJT_NCA189_BR3_SJB2", "PER_NE_CAJT_NCA189_SJB2_BR3"))
+plates_10 = plates_10 %>% 
+  filter(
+    `Sensor Name` %in% c("PER_NE_CAJT_NCA189_SJB2_SJB1", "PER_NE_CAJT_NCA189_BR3_GR",
+                         "PER_NE_CAJT_NCA189_BR3_SJB2", "PER_NE_CAJT_NCA189_SJB2_BR3"))
+plates_11 = plates_11 %>% 
+  filter(
+    `Sensor Name` %in% c("PER_NE_CAJT_NCA189_SJB2_SJB1", "PER_NE_CAJT_NCA189_BR3_GR",
+                         "PER_NE_CAJT_NCA189_BR3_SJB2", "PER_NE_CAJT_NCA189_SJB2_BR3"))
+plates_12 = plates_12 %>% 
+  filter(
+    `Sensor Name` %in% c("PER_NE_CAJT_NCA189_SJB2_SJB1", "PER_NE_CAJT_NCA189_BR3_GR",
+                         "PER_NE_CAJT_NCA189_BR3_SJB2", "PER_NE_CAJT_NCA189_SJB2_BR3"))
+half_season = bind_rows(plates_8, plates_9, plates_10, plates_11, plates_12)
 
-# Get full list of home match days
+# All home match days 21-22 season
+match_days = half_season %>% 
+  filter(day == "2021-08-07" | day == "2021-08-15" | day == "2021-08-25" | day == "2021-08-28" | day == "2021-09-17"
+         | day == "2021-10-17" | day == "2021-10-30" | day == "2021-11-20" | day == "2021-11-30" | day == "2021-12-04"
+         | day == "2021-12-19" | day == "2021-12-27")
+match_days = match_days %>% 
+  filter(day_of_week == "Sunday" | day_of_week == "Saturday")
 
-match_1 = nearby %>% 
-  filter(day == "2021-05-14")
-
-# Check days without matches
-no_match = nearby %>% 
-  filter(! day == "2021-05-14",
-         day_of_week == "Friday")
+# Days without home matches
+other_days = half_season %>% 
+  filter(day != "2021-08-07" & day != "2021-08-15" & day != "2021-08-25" & day != "2021-08-28" & day != "2021-09-17"
+                  & day != "2021-10-17" & day != "2021-10-30" & day != "2021-11-20" & day != "2021-11-30" & day != "2021-12-04"
+                  & day != "2021-12-19" & day != "2021-12-27")
+other_days = other_days %>% 
+  filter(day_of_week == "Sunday" | day_of_week == "Saturday")
 
 # Match day
-sensor_hour = match_1 %>% 
+sensor_hour = match_days %>% 
   st_drop_geometry() %>% 
   group_by(`Sensor Name`, day, hour) %>% 
   summarise(
@@ -50,17 +73,15 @@ sensor_hour = match_1 %>%
 #     ! count < 10
 #   )
 
-hour_group = sensor_hour %>% 
+hour_match = sensor_hour %>% 
   group_by(hour) %>% 
   summarise(days = length(unique(day)),
     daily_plates = sum(plates)/days
     )
 
-ggplot(hour_group, aes(hour, daily_plates)) +
-  geom_line()
 
 # Other days
-sensor_hour = no_match %>% 
+sensor_hour = other_days %>% 
   st_drop_geometry() %>% 
   group_by(`Sensor Name`, day, hour) %>% 
   summarise(
@@ -69,14 +90,16 @@ sensor_hour = no_match %>%
     max_plates = max(Value)
   )
 
-hour_group = sensor_hour %>% 
+hour_other = sensor_hour %>% 
   group_by(hour) %>% 
   summarise(days = length(unique(day)),
             daily_plates = sum(plates)/days
   )
 
-ggplot(hour_group, aes(hour, daily_plates)) +
-  geom_line()
+ggplot(hour_other, aes(hour, daily_plates)) +
+  geom_line(col = "black") +
+  ggplot(hour_match, aes(hour, daily_plates)) +
+  geom_line(col = "red")
 
 
 
