9320b980632eb060f62c5b5860c2086f995e6239
joeytalbot <joeytalbot85@gmail.com>
2023-02-28 15:10:45 +0000

    osm with median

--- code/air-pollution.R
+++ code/air-pollution.R
@@ -10,49 +10,12 @@ pm10_records = readRDS("data/pm10_2021_2.Rds")
 traffic = traffic %>% 
   mutate(`Sum plates` = sum_plates)
 
-# OSM data ----------------------------------------------------------------
-
-# Check the number of lanes, road names, road refs, speed limits, etc
-# Use number of lanes to improve the correlation with 2011 Census data?
-# Need to consider whether road is dual carriageway and whether it has bus lanes, 
-# and how many lanes the camera covers
-
-osm = readRDS("data/osm_drive_2023-01-17.Rds")
-
-# Prevent false matches with minor roads
-osm_nores = osm %>% 
-  filter(
-    !highway == "service",
-    !highway == "unclassified",
-    !highway == "residential"
-    )
-osm_nounc = osm %>% 
-  filter(
-    !highway == "service",
-    !highway == "unclassified"
-  )
-
-
-# Join OSM with plates in
-nearest = st_nearest_feature(traffic, osm_nores)
-nn = osm_nores[nearest,] %>% 
-  st_drop_geometry()
-traffic_osm = bind_cols(traffic, nn)
-
-tm_shape(traffic_osm) + tm_dots("highway")
-
-# Join OSM with PM10
-nearest = st_nearest_feature(pm10, osm_nounc)
-np = osm_nounc[nearest,] %>% 
-  st_drop_geometry()
-pm10_osm = bind_cols(pm10, np)
-tm_shape(pm10_osm) + tm_dots("highway")
 
 # Get median daily traffic for the month ----------------------------------
 
 # Sum traffic each day
 traffic_detailed = readRDS("data/plates_in_2021_2.Rds")
-  
+
 traffic_daily = traffic_detailed %>% 
   st_drop_geometry() %>% 
   group_by(`Sensor Name`, day) %>% 
@@ -81,6 +44,45 @@ traffic_median = left_join(monthly_median, sensor_locations, by = "Sensor Name")
 traffic_median = st_as_sf(traffic_median)
 st_crs(traffic_median) = 4326
 
+
+
+# OSM data ----------------------------------------------------------------
+
+# Check the number of lanes, road names, road refs, speed limits, etc
+# Use number of lanes to improve the correlation with 2011 Census data?
+# Need to consider whether road is dual carriageway and whether it has bus lanes, 
+# and how many lanes the camera covers
+
+osm = readRDS("data/osm_drive_2023-01-17.Rds")
+
+# Prevent false matches with minor roads
+osm_nores = osm %>% 
+  filter(
+    !highway == "service",
+    !highway == "unclassified",
+    !highway == "residential"
+    )
+osm_nounc = osm %>% 
+  filter(
+    !highway == "service",
+    !highway == "unclassified"
+  )
+
+
+# Join OSM with plates in
+nearest = st_nearest_feature(traffic_median, osm_nores)
+nn = osm_nores[nearest,] %>% 
+  st_drop_geometry()
+traffic_osm = bind_cols(traffic_median, nn)
+
+tm_shape(traffic_osm) + tm_dots("highway")
+
+# Join OSM with PM10
+nearest = st_nearest_feature(pm10, osm_nounc)
+np = osm_nounc[nearest,] %>% 
+  st_drop_geometry()
+pm10_osm = bind_cols(pm10, np)
+tm_shape(pm10_osm) + tm_dots("highway")
 
 
 # Checking extreme values -------------------------------------------------
@@ -163,8 +165,10 @@ tm_shape(join_traffic) + tm_polygons("mean_pm10") +
   tm_shape(pm10) + tm_dots("median_value")
 
 # Figure 5
+traffic_fig = traffic_median %>% 
+  rename(`Median daily traffic` = median_traffic)
 tm_shape(join_traffic) + tm_polygons("Pop density", alpha = 0.5) +
-  tm_shape(traffic_median) + tm_dots("median_traffic", palette = "-magma") + 
+  tm_shape(traffic_fig) + tm_dots("Median daily traffic", palette = "-magma") + 
   tm_shape(pm10) + tm_bubbles("median_value")
 
 ggplot(join_traffic, aes(mean_traffic, mean_pm10)) + 
@@ -207,7 +211,7 @@ summary(m1)$r.squared
 traffic_ref_mean = traffic_osm %>% 
   st_drop_geometry() %>% 
   group_by(ref) %>% 
-  summarise(mean_traffic = mean(sum_plates)) %>% 
+  summarise(mean_traffic = mean(median_traffic)) %>% 
   filter(! is.na(ref))
 pm10_ref_mean = pm10_osm %>% 
   st_drop_geometry() %>% 
@@ -220,7 +224,7 @@ ggplot(by_ref, aes(mean_traffic, mean_pm10)) +
   geom_point()
 m1 = lm(mean_pm10 ~ mean_traffic, data = by_ref)
 summary(m1)$r.squared
-# [1] 0.1020857
+# [1] 0.09644407
 
 pm10_missing = pm10_osm %>%
   filter(!ref %in% by_ref$ref)
