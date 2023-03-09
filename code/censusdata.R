56abf0a8acfef1361331c434b086502cde6bceba
Joey Talbot <joeytalbot85@gmail.com>
2023-03-09 15:04:32 +0000

    simplify

--- code/censusdata.R
+++ code/censusdata.R
@@ -220,194 +220,194 @@ tm_shape(car_rnet) +
 
 # Plates In ---------------------------------------------------------------
 
-plates_in_2021 = read_csv("data/2021-2-Plates In.csv")
-
-# Remove bus sensors and test/dummy sensors
-plates_in_2021 = plates_in_2021 %>% 
-  filter(str_detect(pattern = "BUS", `Sensor Name`) == FALSE) %>% 
-  filter(str_detect(pattern = "DUMMY", `Sensor Name`) == FALSE) %>% 
-  filter(str_detect(pattern = "TEST", `Sensor Name`) == FALSE)
-
-plates_in_2021 = plates_in_2021 %>% 
-  mutate(coords = sub(pattern = ",.*", replacement = "", `Location (WKT)`),
-         coords = sub(pattern = ".*\\(", replacement = "", coords))
-plates_in_2021 = plates_in_2021 %>% 
-  mutate(long = sub(pattern = " .*", replacement = "", coords),
-         lat = sub(pattern = ".* ", replacement = "", coords),
-         day = as.Date(Timestamp))
-plates_in_2021 = st_as_sf(plates_in_2021, coords = c("long", "lat"))
-st_crs(plates_in_2021) = 4326
-
-# Find out which days have incomplete data
-in_day = plates_in_2021 %>% 
-  st_drop_geometry() %>% 
-  group_by(`Sensor Name`, day) %>% 
-  summarise(cars_day = sum(Value))
-in_max = in_day %>% 
-  group_by(`Sensor Name`) %>% 
-  summarise(day_max = max(cars_day),
-            day_medi = median(cars_day)) 
-in_sensor_days = inner_join(in_day, in_max, by = "Sensor Name")
-in_full_days = in_sensor_days %>%
-  filter(
-    # cars_day > (day_max/5), # only include days with full records
-    day >= "2021-01-25", # only include days with full records
-    day_medi > 0 # exclude sensors with 0 cars on most days
-    )
-
-# # some extra days are missing
-# jj = inner_join(plates_in_2021, in_full_days, by = c("Sensor Name", "day")) 
-# min(jj$day)
-# daysen = in_full_days %>% 
-#   group_by(`Sensor Name`) %>% 
-#   summarise(start = min(day))
-# xx = jj %>% st_drop_geometry() %>% group_by(`Sensor Name`) %>% summarise(dayss = length(unique(day)))
-# summary(xx$dayss)
-# # Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
-# # 6.000   7.000   7.000   6.991   7.000   7.000 
-# xx %>% filter(dayss == 6)
-# # 1 PER_NE_CAJT_GHA184_NR3_NR2       6
-# # 2 PER_NE_CAJT_NTA189_MF8_SPR6B     6
-# View(in_sensor_days %>% filter(`Sensor Name` == "PER_NE_CAJT_GHA184_NR3_NR2" | `Sensor Name` == "PER_NE_CAJT_NTA189_MF8_SPR6B"))
-# # Both have low traffic on the final sunday
-
-plates_in_2021 = inner_join(plates_in_2021, in_full_days, by = c("Sensor Name", "day")) 
-
-plates_in_2021 = plates_in_2021 %>% 
-  mutate(day_of_week = weekdays(as.Date(Timestamp)),
-         time = hms::as_hms(Timestamp),
-         hour = hour(time))
-
-saveRDS(plates_in_2021, "data/plates_in_2021_1.Rds")
-# needs calibrating to avoid time-sensitive analyses during periods with outlying high values
-
-plates_in_2021 = readRDS("data/plates_in_2021_2.Rds")
-
-in_sd_mean = plates_in_2021 %>% 
-  st_drop_geometry() %>% 
-  group_by(`Sensor Name`) %>% 
-  summarise(n = n(),
-            mean_reading = mean(Value),
-            sd_reading = sd(Value)
-  )
-
-# Data quality checking
-plates_in_sd = inner_join(plates_in_2021, in_sd_mean, by = "Sensor Name")
-# remove extreme outliers (parked cars?) - not required:
-# plates_in_corrected = plates_in_sd %>% 
-#   mutate(Value = case_when(Value > (mean_reading + 6 * sd_reading) ~ mean_reading, 
-#                            TRUE ~ Value)
-#          )
-
-# # testing:
-# test = plates_in_sd %>% 
-#   filter(`Sensor Name` == "PER_NE_CAJT_GHA167_DR3_DR2")
-# tt = test %>% 
+# plates_in_2021 = read_csv("data/2021-2-Plates In.csv")
+# 
+# # Remove bus sensors and test/dummy sensors
+# plates_in_2021 = plates_in_2021 %>% 
+#   filter(str_detect(pattern = "BUS", `Sensor Name`) == FALSE) %>% 
+#   filter(str_detect(pattern = "DUMMY", `Sensor Name`) == FALSE) %>% 
+#   filter(str_detect(pattern = "TEST", `Sensor Name`) == FALSE)
+# 
+# plates_in_2021 = plates_in_2021 %>% 
+#   mutate(coords = sub(pattern = ",.*", replacement = "", `Location (WKT)`),
+#          coords = sub(pattern = ".*\\(", replacement = "", coords))
+# plates_in_2021 = plates_in_2021 %>% 
+#   mutate(long = sub(pattern = " .*", replacement = "", coords),
+#          lat = sub(pattern = ".* ", replacement = "", coords),
+#          day = as.Date(Timestamp))
+# plates_in_2021 = st_as_sf(plates_in_2021, coords = c("long", "lat"))
+# st_crs(plates_in_2021) = 4326
+# 
+# # Find out which days have incomplete data
+# in_day = plates_in_2021 %>% 
 #   st_drop_geometry() %>% 
-#   group_by(day) %>% 
-#   summarise(sum = sum(Value))
-# View(tt)
-
-# Use weekday peak hours only - but this reduces the r squared
-plates_in_peak = plates_in_sd %>% 
-  filter(!(day_of_week == "Sunday" | day_of_week == "Saturday")
-         , hour %in% c(7,8,9,16,17,18)
-  )
-
-
-in_group = plates_in_sd %>% 
-# in_sum = plates_in_peak %>% 
-  st_drop_geometry() %>% 
-  group_by(`Sensor Name`) %>% 
-  summarise(cars = sum(Value))
-sensor_locations = plates_in_sd %>% 
-  select(`Sensor Name`) %>% 
-  group_by(`Sensor Name`) %>% 
-  filter(row_number() == 1)
-in_sum = left_join(in_group, sensor_locations, by = "Sensor Name")
-in_sum = st_as_sf(in_sum)
-st_crs(in_sum) = 4326
-
-filename = paste0("data/in_sum_", 2, ".Rds")
-saveRDS(in_sum, filename)
-
-# Map sensor locations
-length(unique(plates_in_sd$day))
-in_map = in_sum %>% 
-  mutate(`Mean daily plates in` = cars/28)
-tm_shape(in_map) + tm_dots("Mean daily plates in", size = 0.08)
-tm_shape(in_sum) + tm_dots("cars")
+#   group_by(`Sensor Name`, day) %>% 
+#   summarise(cars_day = sum(Value))
+# in_max = in_day %>% 
+#   group_by(`Sensor Name`) %>% 
+#   summarise(day_max = max(cars_day),
+#             day_medi = median(cars_day)) 
+# in_sensor_days = inner_join(in_day, in_max, by = "Sensor Name")
+# in_full_days = in_sensor_days %>%
+#   filter(
+#     # cars_day > (day_max/5), # only include days with full records
+#     day >= "2021-01-25", # only include days with full records
+#     day_medi > 0 # exclude sensors with 0 cars on most days
+#     )
+# 
+# # # some extra days are missing
+# # jj = inner_join(plates_in_2021, in_full_days, by = c("Sensor Name", "day")) 
+# # min(jj$day)
+# # daysen = in_full_days %>% 
+# #   group_by(`Sensor Name`) %>% 
+# #   summarise(start = min(day))
+# # xx = jj %>% st_drop_geometry() %>% group_by(`Sensor Name`) %>% summarise(dayss = length(unique(day)))
+# # summary(xx$dayss)
+# # # Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
+# # # 6.000   7.000   7.000   6.991   7.000   7.000 
+# # xx %>% filter(dayss == 6)
+# # # 1 PER_NE_CAJT_GHA184_NR3_NR2       6
+# # # 2 PER_NE_CAJT_NTA189_MF8_SPR6B     6
+# # View(in_sensor_days %>% filter(`Sensor Name` == "PER_NE_CAJT_GHA184_NR3_NR2" | `Sensor Name` == "PER_NE_CAJT_NTA189_MF8_SPR6B"))
+# # # Both have low traffic on the final sunday
+# 
+# plates_in_2021 = inner_join(plates_in_2021, in_full_days, by = c("Sensor Name", "day")) 
+# 
+# plates_in_2021 = plates_in_2021 %>% 
+#   mutate(day_of_week = weekdays(as.Date(Timestamp)),
+#          time = hms::as_hms(Timestamp),
+#          hour = hour(time))
+# 
+# saveRDS(plates_in_2021, "data/plates_in_2021_1.Rds")
+# # needs calibrating to avoid time-sensitive analyses during periods with outlying high values
+# 
+# plates_in_2021 = readRDS("data/plates_in_2021_2.Rds")
+# 
+# in_sd_mean = plates_in_2021 %>% 
+#   st_drop_geometry() %>% 
+#   group_by(`Sensor Name`) %>% 
+#   summarise(n = n(),
+#             mean_reading = mean(Value),
+#             sd_reading = sd(Value)
+#   )
+# 
+# # Data quality checking
+# plates_in_sd = inner_join(plates_in_2021, in_sd_mean, by = "Sensor Name")
+# # remove extreme outliers (parked cars?) - not required:
+# # plates_in_corrected = plates_in_sd %>% 
+# #   mutate(Value = case_when(Value > (mean_reading + 6 * sd_reading) ~ mean_reading, 
+# #                            TRUE ~ Value)
+# #          )
+# 
+# # # testing:
+# # test = plates_in_sd %>% 
+# #   filter(`Sensor Name` == "PER_NE_CAJT_GHA167_DR3_DR2")
+# # tt = test %>% 
+# #   st_drop_geometry() %>% 
+# #   group_by(day) %>% 
+# #   summarise(sum = sum(Value))
+# # View(tt)
+# 
+# # Use weekday peak hours only - but this reduces the r squared
+# plates_in_peak = plates_in_sd %>% 
+#   filter(!(day_of_week == "Sunday" | day_of_week == "Saturday")
+#          , hour %in% c(7,8,9,16,17,18)
+#   )
+# 
+# 
+# in_group = plates_in_sd %>% 
+# # in_sum = plates_in_peak %>% 
+#   st_drop_geometry() %>% 
+#   group_by(`Sensor Name`) %>% 
+#   summarise(cars = sum(Value))
+# sensor_locations = plates_in_sd %>% 
+#   select(`Sensor Name`) %>% 
+#   group_by(`Sensor Name`) %>% 
+#   filter(row_number() == 1)
+# in_sum = left_join(in_group, sensor_locations, by = "Sensor Name")
+# in_sum = st_as_sf(in_sum)
+# st_crs(in_sum) = 4326
+# 
+# filename = paste0("data/in_sum_", 2, ".Rds")
+# saveRDS(in_sum, filename)
+# 
+# # Map sensor locations
+# length(unique(plates_in_sd$day))
+# in_map = in_sum %>% 
+#   mutate(`Mean daily plates in` = cars/28)
+# tm_shape(in_map) + tm_dots("Mean daily plates in", size = 0.08)
+# tm_shape(in_sum) + tm_dots("cars")
 
 
 # Plates Out --------------------------------------------------------------
 
-plates_out_2021 = read_csv("data/2021-2-Plates Out.csv")
-
-# Remove bus sensors and test/dummy sensors
-plates_out_2021 = plates_out_2021 %>% 
-  filter(str_detect(pattern = "BUS", `Sensor Name`) == FALSE) %>% 
-  filter(str_detect(pattern = "DUMMY", `Sensor Name`) == FALSE) %>% 
-  filter(str_detect(pattern = "TEST", `Sensor Name`) == FALSE)
-
-plates_out_2021 = plates_out_2021 %>% 
-  mutate(coords = sub(pattern = ".*, ", replacement = "", `Location (WKT)`),
-         coords = sub(pattern = "\\).*", replacement = "", coords))
-plates_out_2021 = plates_out_2021 %>% 
-  mutate(long = sub(pattern = " .*", replacement = "", coords),
-         lat = sub(pattern = ".* ", replacement = "", coords),
-         day = as.Date(Timestamp))
-plates_out_2021 = st_as_sf(plates_out_2021, coords = c("long", "lat"))
-st_crs(plates_out_2021) = 4326
-
-# Find out which days have incomplete data
-out_day = plates_out_2021 %>% 
-  st_drop_geometry() %>% 
-  group_by(`Sensor Name`, day) %>% 
-  summarise(cars_day = sum(Value))
-out_max = out_day %>% 
-  group_by(`Sensor Name`) %>% 
-  summarise(day_max = max(cars_day),
-            day_medi = median(cars_day)) 
-out_sensor_days = inner_join(out_day, out_max, by = "Sensor Name")
-out_full_days = out_sensor_days %>%
-  filter(
-    # cars_day > (day_max/5), # only include days with full records
-    day >= "2021-01-25", # only include days with full records
-    day_medi > 0 # exclude sensors with 0 cars on most days
-  )
-
-plates_out_2021 = inner_join(plates_out_2021, out_full_days, by = c("Sensor Name", "day")) 
-
-plates_out_2021 = plates_out_2021 %>% 
-  mutate(day_of_week = weekdays(as.Date(Timestamp)),
-         time = hms::as_hms(Timestamp),
-         hour = hour(time))
-
-saveRDS(plates_out_2021, "data/plates_out_2021_1.Rds")
-
-plates_out_2021 = readRDS("data/plates_out_2021_1.Rds")
-
-# Use weekday peak hours only - but this reduces the r squared
-plates_out_peak = plates_out_2021 %>% 
-  filter(!(day_of_week == "Sunday" | day_of_week == "Saturday")
-         , hour %in% c(7,8,9,16,17,18)
-  )
-
-# needs calibrating to avoid outlying high values due to parked cars
-out_sum = plates_out_2021 %>% 
-  group_by(`Sensor Name`) %>% 
-  summarise(cars = sum(Value), 
-            n = n(),
-            mean_reading = mean(Value)
-  )
-
-saveRDS(out_sum, "data/out_sum.Rds")
-tm_shape(out_sum) + tm_dots("cars")
-
-
-# Read in 
-in_sum = readRDS("data/in_sum.Rds")
-out_sum = readRDS("data/out_sum.Rds")
+# plates_out_2021 = read_csv("data/2021-2-Plates Out.csv")
+# 
+# # Remove bus sensors and test/dummy sensors
+# plates_out_2021 = plates_out_2021 %>% 
+#   filter(str_detect(pattern = "BUS", `Sensor Name`) == FALSE) %>% 
+#   filter(str_detect(pattern = "DUMMY", `Sensor Name`) == FALSE) %>% 
+#   filter(str_detect(pattern = "TEST", `Sensor Name`) == FALSE)
+# 
+# plates_out_2021 = plates_out_2021 %>% 
+#   mutate(coords = sub(pattern = ".*, ", replacement = "", `Location (WKT)`),
+#          coords = sub(pattern = "\\).*", replacement = "", coords))
+# plates_out_2021 = plates_out_2021 %>% 
+#   mutate(long = sub(pattern = " .*", replacement = "", coords),
+#          lat = sub(pattern = ".* ", replacement = "", coords),
+#          day = as.Date(Timestamp))
+# plates_out_2021 = st_as_sf(plates_out_2021, coords = c("long", "lat"))
+# st_crs(plates_out_2021) = 4326
+# 
+# # Find out which days have incomplete data
+# out_day = plates_out_2021 %>% 
+#   st_drop_geometry() %>% 
+#   group_by(`Sensor Name`, day) %>% 
+#   summarise(cars_day = sum(Value))
+# out_max = out_day %>% 
+#   group_by(`Sensor Name`) %>% 
+#   summarise(day_max = max(cars_day),
+#             day_medi = median(cars_day)) 
+# out_sensor_days = inner_join(out_day, out_max, by = "Sensor Name")
+# out_full_days = out_sensor_days %>%
+#   filter(
+#     # cars_day > (day_max/5), # only include days with full records
+#     day >= "2021-01-25", # only include days with full records
+#     day_medi > 0 # exclude sensors with 0 cars on most days
+#   )
+# 
+# plates_out_2021 = inner_join(plates_out_2021, out_full_days, by = c("Sensor Name", "day")) 
+# 
+# plates_out_2021 = plates_out_2021 %>% 
+#   mutate(day_of_week = weekdays(as.Date(Timestamp)),
+#          time = hms::as_hms(Timestamp),
+#          hour = hour(time))
+# 
+# saveRDS(plates_out_2021, "data/plates_out_2021_1.Rds")
+# 
+# plates_out_2021 = readRDS("data/plates_out_2021_1.Rds")
+# 
+# # Use weekday peak hours only - but this reduces the r squared
+# plates_out_peak = plates_out_2021 %>% 
+#   filter(!(day_of_week == "Sunday" | day_of_week == "Saturday")
+#          , hour %in% c(7,8,9,16,17,18)
+#   )
+# 
+# # needs calibrating to avoid outlying high values due to parked cars
+# out_sum = plates_out_2021 %>% 
+#   group_by(`Sensor Name`) %>% 
+#   summarise(cars = sum(Value), 
+#             n = n(),
+#             mean_reading = mean(Value)
+#   )
+# 
+# saveRDS(out_sum, "data/out_sum.Rds")
+# tm_shape(out_sum) + tm_dots("cars")
+# 
+# 
+# # Read in 
+# in_sum = readRDS("data/in_sum.Rds")
+# out_sum = readRDS("data/out_sum.Rds")
 
 # these are in the same location but have different car counts:
 per = plates_in_sd %>% 
@@ -462,16 +462,17 @@ inn = in_sum %>%
 map_net = car_rnet %>% 
   mutate(`Commute route network` = all_vehs) %>% 
   arrange(all_vehs) # so the high flow routes are plotted on top
-tm_shape(map_net) + tm_lines("Commute route network", 
-                             breaks = c(0, 500, 1000, 2000, 5000, 15000))
+# tm_shape(map_net) + tm_lines("Commute route network", 
+#                              breaks = c(0, 500, 1000, 2000, 5000, 15000))
+
 # Figure 2
 tm_shape(map_net) + 
   tm_lines("Commute route network", 
            breaks = c(0, 500, 1000, 2000, 5000, 15000), lwd = 1.3) + 
   tm_shape(inn) + tm_dots("Mean daily plates in", size = 0.08, palette = "-magma")
 
-tm_shape(car_2013_sum) + tm_dots() +
-  tm_shape(car_rnet) + tm_lines("all_vehs")
+# tm_shape(car_2013_sum) + tm_dots() +
+#   tm_shape(car_rnet) + tm_lines("all_vehs")
 
 # # Speed and flow (small area only)
 # speed_mean = average_speed_2021 %>% 
