493d9b7b78b4a2f226643b63de1d11bdcd25e0e8
Joey Talbot <joeytalbot85@gmail.com>
2023-03-15 10:50:39 +0000

    air pollution unused code

--- code/air-pollution.R
+++ code/air-pollution.R
@@ -128,15 +128,8 @@ per = traffic_osm %>%
 
 library(pct)
 northeast = get_pct_zones("north-east", geography = "msoa")
-newc_msoa = northeast %>% 
-  filter(lad_name == "Newcastle upon Tyne")
-tm_shape(newc_msoa) + tm_polygons() + 
-  tm_shape(pm10) + tm_dots()
 
-northeast_lsoa = get_pct_zones("north-east", geography = "lsoa")
-northeast_lsoa = st_make_valid(northeast_lsoa)
-tm_shape(northeast_lsoa) + tm_polygons()
-
+# Population density
 pop = read_csv("data/popn-2020.csv")
 pop = pop %>% 
   rename(population = `All Ages`)
@@ -181,30 +174,6 @@ summary(m2)$r.squared
 summary(m2)
 # [1] 0.05794636
 
-# Same with LSOAs ---------------------------------------------------------
-
-# only 14 LSOAs match both datasets
-
-join_lsoa = st_join(northeast_lsoa, pm10_osm) %>%
-  group_by(geo_code) %>%
-  summarise(mean_pm10 = mean(median_value)) %>%
-  filter(!is.na(mean_pm10))
-tm_shape(join_lsoa) + tm_polygons("mean_pm10")
-join_lsoa_traffic = st_join(join_lsoa, traffic_osm) %>%
-  group_by(geo_code, mean_pm10) %>%
-  summarise(mean_traffic = mean(sum_plates)) %>%
-  filter(!is.na(mean_traffic))
-tm_shape(join_lsoa_traffic) + tm_polygons("mean_pm10")
-tm_shape(join_lsoa_traffic) + tm_polygons("mean_traffic")
-
-ggplot(join_lsoa_traffic, aes(mean_traffic, mean_pm10)) + 
-  geom_point()
-m1 = lm(mean_pm10 ~ mean_traffic, data = join_lsoa_traffic)
-summary(m1)$r.squared
-# [1] 0.02452623
-
-# Get built-up area bounds or calculate population density
-
 
 # Sensors on same road ----------------------------------------------------
 
@@ -226,62 +195,6 @@ m1 = lm(mean_pm10 ~ mean_traffic, data = by_ref)
 summary(m1)$r.squared
 # [1] 0.09644407
 
-pm10_missing = pm10_osm %>%
-  filter(!ref %in% by_ref$ref)
+# pm10_missing = pm10_osm %>%
+#   filter(!ref %in% by_ref$ref)
 
-# traffic_name_mean = traffic_osm %>% 
-#   st_drop_geometry() %>% 
-#   group_by(name) %>% 
-#   summarise(mean_traffic = mean(sum_plates)) %>% 
-#   filter(! is.na(name))
-# by_name = inner_join(pm10_missing, traffic_name_mean, by = "name")
-# by_ref_name = bind_rows(by_ref, by_name)
-# 
-# ggplot(by_ref_name, aes(mean_traffic, median_value)) + 
-#   geom_point()
-# m1 = lm(median_value ~ mean_traffic, data = by_ref_name)
-# summary(m1)$r.squared
-# # [1] 0.0388554
-
-# Both MSOA and road ref
-join = st_join(northeast, pm10_missing) %>% 
-  group_by(geo_code) %>% 
-  summarise(mean_pm10 = mean(median_value)) %>% 
-  filter(!is.na(mean_pm10))
-tm_shape(join) + tm_polygons("mean_pm10")
-join_traffic = st_join(join, traffic_osm) %>% 
-  group_by(geo_code, mean_pm10) %>% 
-  summarise(mean_traffic = mean(sum_plates)) %>% 
-  filter(!is.na(mean_traffic))
-tm_shape(join_traffic) + tm_polygons("mean_pm10") + 
-  tm_shape(pm10) + tm_dots("median_value")
-tm_shape(join_traffic) + tm_polygons("mean_pm10", alpha = 0.5) +
-  tm_shape(traffic) + tm_dots("sum_plates", palette = "-magma") + 
-  tm_shape(pm10) + tm_bubbles("median_value")
-
-ggplot(join_traffic, aes(mean_traffic, mean_pm10)) + 
-  geom_point()
-m1 = lm(mean_pm10 ~ mean_traffic, data = join_traffic)
-summary(m1)$r.squared
-# [1] 0.1479101
-
-
-# Sensors on same road type -----------------------------------------------
-
-traffic_highway_mean = traffic_osm %>% 
-  st_drop_geometry() %>% 
-  group_by(highway) %>% 
-  summarise(mean_traffic = mean(sum_plates)) %>% 
-  filter(! is.na(highway))
-pm10_highway_mean = pm10_osm %>% 
-  st_drop_geometry() %>% 
-  group_by(highway) %>% 
-  summarise(mean_pm10 = mean(median_value)) %>% 
-  filter(! is.na(highway))
-by_highway = inner_join(pm10_highway_mean, traffic_highway_mean, by = "highway")
-
-ggplot(by_highway, aes(mean_traffic, mean_pm10)) + 
-  geom_point()
-m1 = lm(mean_pm10 ~ mean_traffic, data = by_highway)
-summary(m1)$r.squared
-# [1] 0.07241296
---
+++ code/extras-airpollution.R
@@ -0,0 +1,91 @@
+
+# newc_msoa = northeast %>% 
+#   filter(lad_name == "Newcastle upon Tyne")
+# tm_shape(newc_msoa) + tm_polygons() + 
+#   tm_shape(pm10) + tm_dots()
+
+# northeast_lsoa = get_pct_zones("north-east", geography = "lsoa")
+# northeast_lsoa = st_make_valid(northeast_lsoa)
+# tm_shape(northeast_lsoa) + tm_polygons()
+
+# # Same with LSOAs ---------------------------------------------------------
+# 
+# # only 14 LSOAs match both datasets
+# 
+# join_lsoa = st_join(northeast_lsoa, pm10_osm) %>%
+#   group_by(geo_code) %>%
+#   summarise(mean_pm10 = mean(median_value)) %>%
+#   filter(!is.na(mean_pm10))
+# tm_shape(join_lsoa) + tm_polygons("mean_pm10")
+# join_lsoa_traffic = st_join(join_lsoa, traffic_osm) %>%
+#   group_by(geo_code, mean_pm10) %>%
+#   summarise(mean_traffic = mean(sum_plates)) %>%
+#   filter(!is.na(mean_traffic))
+# tm_shape(join_lsoa_traffic) + tm_polygons("mean_pm10")
+# tm_shape(join_lsoa_traffic) + tm_polygons("mean_traffic")
+# 
+# ggplot(join_lsoa_traffic, aes(mean_traffic, mean_pm10)) + 
+#   geom_point()
+# m1 = lm(mean_pm10 ~ mean_traffic, data = join_lsoa_traffic)
+# summary(m1)$r.squared
+# # [1] 0.02452623
+
+
+# Sensors on same road ----------------------------------------------------
+
+# traffic_name_mean = traffic_osm %>% 
+#   st_drop_geometry() %>% 
+#   group_by(name) %>% 
+#   summarise(mean_traffic = mean(sum_plates)) %>% 
+#   filter(! is.na(name))
+# by_name = inner_join(pm10_missing, traffic_name_mean, by = "name")
+# by_ref_name = bind_rows(by_ref, by_name)
+# 
+# ggplot(by_ref_name, aes(mean_traffic, median_value)) + 
+#   geom_point()
+# m1 = lm(median_value ~ mean_traffic, data = by_ref_name)
+# summary(m1)$r.squared
+# # [1] 0.0388554
+
+# # Both MSOA and road ref
+# join = st_join(northeast, pm10_missing) %>% 
+#   group_by(geo_code) %>% 
+#   summarise(mean_pm10 = mean(median_value)) %>% 
+#   filter(!is.na(mean_pm10))
+# tm_shape(join) + tm_polygons("mean_pm10")
+# join_traffic = st_join(join, traffic_osm) %>% 
+#   group_by(geo_code, mean_pm10) %>% 
+#   summarise(mean_traffic = mean(sum_plates)) %>% 
+#   filter(!is.na(mean_traffic))
+# tm_shape(join_traffic) + tm_polygons("mean_pm10") + 
+#   tm_shape(pm10) + tm_dots("median_value")
+# tm_shape(join_traffic) + tm_polygons("mean_pm10", alpha = 0.5) +
+#   tm_shape(traffic) + tm_dots("sum_plates", palette = "-magma") + 
+#   tm_shape(pm10) + tm_bubbles("median_value")
+# 
+# ggplot(join_traffic, aes(mean_traffic, mean_pm10)) + 
+#   geom_point()
+# m1 = lm(mean_pm10 ~ mean_traffic, data = join_traffic)
+# summary(m1)$r.squared
+# # [1] 0.1479101
+# 
+# 
+# # Sensors on same road type -----------------------------------------------
+# 
+# traffic_highway_mean = traffic_osm %>% 
+#   st_drop_geometry() %>% 
+#   group_by(highway) %>% 
+#   summarise(mean_traffic = mean(sum_plates)) %>% 
+#   filter(! is.na(highway))
+# pm10_highway_mean = pm10_osm %>% 
+#   st_drop_geometry() %>% 
+#   group_by(highway) %>% 
+#   summarise(mean_pm10 = mean(median_value)) %>% 
+#   filter(! is.na(highway))
+# by_highway = inner_join(pm10_highway_mean, traffic_highway_mean, by = "highway")
+# 
+# ggplot(by_highway, aes(mean_traffic, mean_pm10)) + 
+#   geom_point()
+# m1 = lm(mean_pm10 ~ mean_traffic, data = by_highway)
+# summary(m1)$r.squared
+# # [1] 0.07241296
